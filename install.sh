e5e17fb9d9be7c1951639b80de5cdafbf9ff1e87
Carlos <carlos.bergamasco@gmail.com>
2023-07-07 18:21:48 -0300

    - Add package manager checking - Add Vimplug instalation

--- install.sh
+++ install.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-function checkInstall(){
-  if [ "`whereis $1`" == "$1:" ]; then
-    echo "-> Installing $1"
-    echo "sudo apt install $*"
+packcmd=''
+
+function checkPackMan(){
+  if [ "`whereis pkg`" != "pkg:" ]; then
+    packcmd="pkg install"
+    echo "-> Using 'pkg' package manager"
+  elif [ "`whereis pacman`" != "pacman:" ]; then
+    packcmd="pacman -S"
+    echo "-> Using 'pacman' package manager"
   else
-    echo "${1^} already installed"
+    packcmd="sudo apt install"
+    echo "-> Using 'apt' package manager"
   fi
 }
 
+function checkInstall(){
+  if [ "`whereis $1`" == "$1:" ]; then
+    echo "-> Installing $1"
+    echo "$packcmd install $*"
+  else
+    echo "-> ${1^} already installed"
+  fi
+}
+
+# check package manager
+checkPackMan
 # install git
 checkInstall "git"
 # install mc
 checkInstall "mc"
 # install neovim
 checkInstall "neovim"
+# install vimplug
+ #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+ #       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # install i3
 checkInstall "i3" "i3lock" "i3status" "i3blocks"
 # install utilities
