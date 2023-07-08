6eda4bd2bb4e81985c339085310a2de2d7341cbd
Carlos <carlos.bergamasco@gmail.com>
2023-07-08 11:14:09 -0300

    Add installation commands to script and new shortcut for i3wm

--- install.sh
+++ install.sh
@@ -18,9 +18,19 @@ function checkPackMan(){
 function checkInstall(){
   if [ "`whereis $1`" == "$1:" ]; then
     echo "-> Installing $1"
-    echo "$packcmd install $*"
+    $packcmd $*
   else
     echo "-> ${1^} already installed"
+  fi
+}
+
+function checkVimplug(){
+  if [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
+    echo "-> Installing vimplug"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  else
+    echo "-> Vimplug already installed"
   fi
 }
 
@@ -28,17 +38,17 @@ function checkInstall(){
 checkPackMan
 # install git
 checkInstall "git"
+# install i3 and utilities
+if [ "$packcmd" != "pkg install" ]; then
+  checkInstall "i3" "i3lock" "i3status" "i3blocks"
+  checkInstall "numlockx" "feh" "flameshot" "ffmpeg" "slop"
+fi
 # install mc
 checkInstall "mc"
 # install neovim
 checkInstall "neovim"
 # install vimplug
- #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
- #       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# install i3
-checkInstall "i3" "i3lock" "i3status" "i3blocks"
-# install utilities
-checkInstall "numlockx" "feh" "flameshot" "ffmpeg" "slop"
+checkVimplug
 
 # install vim plugins
-echo "vim -c 'PlugInstall'"
+vim -c 'PlugInstall'
